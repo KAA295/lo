dbcd42e6b4c846ba374664f811963da9c029fb06
KAA295 <alekseikidiaev@yandex.ru>
2025-08-16 05:43:11 +0400

    fix

--- api/rest/types/errors.go
+++ api/rest/types/errors.go
@@ -12,7 +12,7 @@ type ErrorResponse struct {
 func SendError(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	json.NewEncoder(w).Encode(ErrorResponse{
+	_ = json.NewEncoder(w).Encode(ErrorResponse{
 		Error: message,
 	})
 }
